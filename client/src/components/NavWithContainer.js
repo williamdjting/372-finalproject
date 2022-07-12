ac1acd185376d685d5b8f20ff313f1403fea224a
Matthew Zegar <matthewzegar@gmail.com>
2022-07-12 16:38:51 -0700

    Remove NavBar file, combine it

--- client/src/components/NavWithContainer.js
+++ client/src/components/NavWithContainer.js
@@ -1,7 +1,36 @@
 import React from 'react';
 import Container from '@mui/material/Container';
-import NavBar from './NavBar';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 import { Outlet } from 'react-router';
+
+function NavBar(props) {
+    function navItems() {
+        return <>
+            <Button color="inherit">Insights</Button>
+            <Button color="inherit">WatchLists</Button>
+            <Button color="inherit">Logout</Button>
+        </>
+    }
+
+    return (
+        <Container maxWidth="lg" sx={{ my: 3 }}>
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="h4" sx={{ flexGrow: 1 }}>
+                            Group Stock
+                        </Typography>
+                        {props.hidden ? <></> : navItems()}
+                    </Toolbar>
+                </AppBar>
+            </Box>
+        </Container>
+    );
+};
 
 export default function NavWithContainer(props) {
     return (
