f85e67f892a308bcafe9f5fb66c6154c60de55d1
wufengw <wufengw@sfu.ca>
2022-07-26 23:20:35 -0700

    added working chart as prototype, reformatted unit values, added new routes, added event listeners for all stock rows/table

--- client/src/pages/dashboard/Dashboard9.js
+++ client/src/pages/dashboard/Dashboard9.js
@@ -1,26 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import {Table, TableBody, TableCell, TableContainer, TableHead,
     TableRow, Paper, Select, MenuItem, InputLabel} from '@mui/material';
+import StockInfoContainer from '../../components/viz/StockInfoContainer';
 
 import '../../stylesheets/Dashboard9.css'
 import  axios  from 'axios';
 
-const MARKET_CAP = 'MarketCapitalization';
-const REVENUE = 'RevenuePerShareTTM';
-const REVENUE_GROWTH = 'QuarterlyRevenueGrowthYOY';
+const MARKET_CAP = 'MarketCapitalization'; // commonStockSharesOutstanding * stock price at given date (balance sheet, income statement)
+const REVENUE = 'RevenuePerShareTTM'; // totalRevenue / commonStockSharesOutstanding
+const REVENUE_GROWTH = 'QuarterlyRevenueGrowthYOY'; 
 const PROFIT_MARGIN = 'ProfitMargin';
-const PE_RATIO = 'PERatio';
-const PS_RATIO = 'PriceToSalesRatioTTM';
-
-function createData1(name, price, revenue, revenueGrowth, psRatio, grossProfit, ebitda, peRatio ) {
-    return { name, price, revenue, revenueGrowth, psRatio, grossProfit, ebitda, peRatio
-  }
-}
-  
-const rows1 = [
-    createData1('Shopify', 35.57, 4600000000,57,28, 0.94,335000000,28.23),
-
-];
+const PE_RATIO = 'PERatio'; //market price per share / reportedEPS
+const PS_RATIO = 'PriceToSalesRatioTTM'; // market cap / total revenue of the last 12 months(1year)
 
 const companyInfoArrSortAndFilter = (objArr, sortType, filterType) => {
     let isAscend = sortType === 'descending' ? false : true;
@@ -90,12 +81,12 @@ function Dashboard9() {
 
     const [companyInfoArr, setCompanyInfoArr] = useState([]);
     const [sortValue, setSortValue] = useState('descending');
+    const [revenuePerShareTS, setRevenuePerShareTS] = useState([]);
 
     const handleRowClick = (e) => {
-        console.log('test')
-        const code = e.currentTarget.getAttribute('data-key');
-        const tableId = e.currentTarget.getAttribute('data-tableId');
-        console.log(code, tableId);
+        const code = e.currentTarget.getAttribute('data-code');
+        const tableid = e.currentTarget.getAttribute('data-tableid');
+        console.log(code, tableid);
     };
 
     const handleSortChange = (e) => {
@@ -104,34 +95,39 @@ function Dashboard9() {
 
     useEffect(() => {
         const fetchData = async () => {
-        const response = await axios.get('/stockquery/getUserStockListFromDb');
-        const objectArr = await response.data;
-        let companyInfoArr = [];
-        for await (let arrObj of objectArr) {
-            const companyInfoRes = await axios.post('/stockquery/companyStockOverview', {
-                companySymbol: arrObj.code  
+            //prototyping
+            const test = await axios.post('/stockquery/getRevenuePerShareTTMTimeSeries', {
+                companySymbol: 'IBM'
             })
-            companyInfoArr.push(await companyInfoRes.data);
-        }
-        setCompanyInfoArr(companyInfoArr);
+            const testData = await test.data;
+            setRevenuePerShareTS(testData)
+            const response = await axios.get('/stockquery/getUserStockListFromDb');
+            const objectArr = await response.data;
+            let companyInfoArr = [];
+            for await (let arrObj of objectArr) {
+                const companyInfoRes = await axios.post('/stockquery/companyStockOverview', {
+                    companySymbol: arrObj.code  
+                })
+                companyInfoArr.push(await companyInfoRes.data);
+            }
+            setCompanyInfoArr(companyInfoArr);
         };
         fetchData();
       }, []);
 
       useEffect(() => {
-        console.log(companyInfoArrSortAndFilter(companyInfoArr, sortValue, PROFIT_MARGIN))
       }, [companyInfoArr]);
 
     return (
         <div>
             <h1 align="center">Your Insights</h1>
             <h2 align="center">[sorting tab, react hook goes here]</h2>
+            <StockInfoContainer rangeData={revenuePerShareTS}/>
             <InputLabel id="sortLabel">Sort</InputLabel>
             <Select labelId="sortLabel" id="sortLabel" value={sortValue} label="sort" onChange={handleSortChange}>
                 <MenuItem value='ascending'>Ascending</MenuItem>
                 <MenuItem value='descending'>Descending</MenuItem>
             </Select>
-
             <table align="center">
                 <tr className='row9'>
                     <td className='col9'>
@@ -148,8 +144,8 @@ function Dashboard9() {
                         <TableBody>
                             {companyInfoArrSortAndFilter(companyInfoArr, sortValue, MARKET_CAP).map((row) => (
                             <TableRow
-                                data-tableId={MARKET_CAP}
-                                data-key={row.Symbol}
+                                data-tableid={MARKET_CAP}
+                                data-code={row.Symbol}
                                 key={row.Symbol}
                                 onClick={handleRowClick}
                                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
@@ -170,7 +166,7 @@ function Dashboard9() {
                         <Table  sx={{ minWidth: 150, maxWidth: 1200}} aria-label="simple table">
                         <TableHead>
                             <TableRow>
-                            <TableCell align="left"><b>Revenue</b></TableCell>
+                            <TableCell align="left"><b>Revenue Per Share(TTM)</b></TableCell>
                             <TableCell align="right"><b></b></TableCell>
                             <TableCell align="right"><b></b></TableCell>
                             </TableRow>
@@ -179,12 +175,15 @@ function Dashboard9() {
                         {companyInfoArrSortAndFilter(companyInfoArr, sortValue, REVENUE).map((row) => (
                             <TableRow
                                 key={row.Symbol}
+                                data-tableid={MARKET_CAP}
+                                data-code={row.Symbol}
+                                onClick={handleRowClick}
                                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                             >
                                 <TableCell align="left" component="th" scope="row">
                                 {row.Name}
                                 </TableCell>
-                                <TableCell align="right">${row.RevenuePerShareTTM}</TableCell>
+                                <TableCell align="right">{row.RevenuePerShareTTM}</TableCell>
                             </TableRow>
                             ))}
                         </TableBody>
@@ -198,7 +197,7 @@ function Dashboard9() {
                         <TableHead>
                             <TableRow>
 
-                            <TableCell align="left"><b>Revenue Growth</b></TableCell>
+                            <TableCell align="left"><b>Quarterly Revenue Growth(YoY)</b></TableCell>
                             <TableCell align="right"><b></b></TableCell>
                             <TableCell align="right"><b></b></TableCell>
 
@@ -208,12 +207,15 @@ function Dashboard9() {
                         {companyInfoArrSortAndFilter(companyInfoArr, sortValue, REVENUE_GROWTH).map((row) => (
                             <TableRow
                                 key={row.Symbol}
+                                data-tableid={MARKET_CAP}
+                                data-code={row.Symbol}
+                                onClick={handleRowClick}
                                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                             >
                                 <TableCell align="left" component="th" scope="row">
                                 {row.Name}
                                 </TableCell>
-                                <TableCell align="right">${row.QuarterlyRevenueGrowthYOY}</TableCell>
+                                <TableCell align="right">{Math.round(row.QuarterlyRevenueGrowthYOY * 10000) / 100}%</TableCell>
                             </TableRow>
                             ))}
                         </TableBody>
@@ -236,12 +238,15 @@ function Dashboard9() {
                         {companyInfoArrSortAndFilter(companyInfoArr, sortValue, PROFIT_MARGIN).map((row) => (
                             <TableRow
                                 key={row.Symbol}
+                                data-tableid={MARKET_CAP}
+                                data-code={row.Symbol}
+                                onClick={handleRowClick}
                                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                             >
                                 <TableCell align="left" component="th" scope="row">
                                 {row.Name}
                                 </TableCell>
-                                <TableCell align="right">${row.ProfitMargin}</TableCell>
+                                <TableCell align="right">{Math.round(row.ProfitMargin * 10000) / 100}%</TableCell>
                             </TableRow>
                             ))}
                         </TableBody>
@@ -263,12 +268,15 @@ function Dashboard9() {
                         {companyInfoArrSortAndFilter(companyInfoArr, sortValue, PE_RATIO).map((row) => (
                             <TableRow
                                 key={row.Symbol}
+                                data-tableid={MARKET_CAP}
+                                data-code={row.Symbol}
+                                onClick={handleRowClick}
                                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                             >
                                 <TableCell align="left" component="th" scope="row">
                                 {row.Name}
                                 </TableCell>
-                                <TableCell align="right">${row.PERatio}</TableCell>
+                                <TableCell align="right">{row.PERatio}</TableCell>
                             </TableRow>
                             ))}
                         </TableBody>
@@ -289,12 +297,15 @@ function Dashboard9() {
                             {companyInfoArrSortAndFilter(companyInfoArr, sortValue, PS_RATIO).map((row) => (
                                 <TableRow
                                     key={row.Symbol}
+                                    data-tableid={MARKET_CAP}
+                                    data-code={row.Symbol}
+                                    onClick={handleRowClick}
                                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                 >
                                     <TableCell align="left" component="th" scope="row">
                                     {row.Name}
                                     </TableCell>
-                                    <TableCell align="right">${row.PriceToSalesRatioTTM}</TableCell>
+                                    <TableCell align="right">{row.PriceToSalesRatioTTM}</TableCell>
                                 </TableRow>
                             ))}
                         </TableBody>
@@ -308,3 +319,34 @@ function Dashboard9() {
 }
 
 export default Dashboard9;
+
+/*
+"fiscalDateEnding": "2021-12-31",
+            "reportedCurrency": "USD",
+            "grossProfit": "31486000000",
+            "totalRevenue": "57350000000",
+            "costOfRevenue": "25865000000",
+            "costofGoodsAndServicesSold": "300000000",
+            "operatingIncome": "4786000000",
+            "sellingGeneralAndAdministrative": "18745000000",
+            "researchAndDevelopment": "6488000000",
+            "operatingExpenses": "26700000000",
+            "investmentIncomeNet": "None",
+            "netInterestIncome": "-1155000000",
+            "interestIncome": "52000000",
+            "interestExpense": "26700000000",
+            "nonInterestIncome": "None",
+            "otherNonOperatingIncome": "-873000000",
+            "depreciation": "3888000000",
+            "depreciationAndAmortization": "2529000000",
+            "incomeBeforeTax": "5867000000",
+            "incomeTaxExpense": "124000000",
+            "interestAndDebtExpense": "1155000000",
+            "netIncomeFromContinuingOperations": "4712000000",
+            "comprehensiveIncomeNetOfTax": "11299000000",
+            "ebit": "7022000000",
+            "ebitda": "9551000000",
+            "netIncome": "5743000000"
+
+            */
+            // (57350000000 - 26700000000 - 26700000000 - 124000000 - 1155000000) / 57350000000
